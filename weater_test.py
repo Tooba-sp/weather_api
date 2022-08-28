fb73fd4abc67114d940ea8e9d3a7dd3c1415d8ad
Tooba Sohail <toobasohail2019@gmail.com>
2022-08-28 15:20:21 -0500

    initail commit

---
+++ weater_test.py
@@ -0,0 +1,49 @@
+
+
+weather = {
+    'Chicago': 90,
+    'Dallas': 100,
+    'New York': 200
+}
+
+
+
+weather = {
+    'IL':
+        {
+            'Lemont': 100,
+            'Skokie': 200,
+            'Niles': -1,
+        }
+    ,
+    'TX': 100,
+    'NY': 200
+}
+
+
+
+weather = {
+    'Chicago':{
+        'Lemont': {
+            'weaterh': 100,
+             'Wind': 200,
+             'Humidity': -1,
+        },
+        'Niles': {
+            'weaterh': 100,
+            'Wind': 200,
+            'Humidity': -1,
+        },
+        'Skokie': {
+            'weaterh': 100,
+            'Wind': 200,
+            'Humidity': -1,
+        }
+    },
+
+    'Dallas': 100,
+    'New York': 200
+}
+
+
+
